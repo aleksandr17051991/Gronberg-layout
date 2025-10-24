52a76465f2cd2058a7756413770b5daee6826ef8
Aleksandr Oliferchuk <oliferchuk.aleksandr@gmail.com>
2025-10-25 01:27:47 +0300

    feat: Header - created burger-menu content

---
+++ gronberg/js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+document.addEventListener('click', documentActions);
+
+function documentActions(element) {
+  const targetElement = element.target;
+  if (targetElement.closest('.menu__icon')) {
+    document.documentElement.classList.toggle('menu-open');
+  }
+}
