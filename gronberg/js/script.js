c5a7ff080b4d310d6b3162dea67154fc8939ab1b
Aleksandr Oliferchuk <oliferchuk.aleksandr@gmail.com>
2025-10-29 22:34:57 +0200

    fix(burger-menu): disable main page scroll when menu is opened

--- gronberg/js/script.js
+++ gronberg/js/script.js
@@ -6,5 +6,6 @@ function documentActions(element) {
   const targetElement = element.target;
   if (targetElement.closest('.menu__icon')) {
     document.documentElement.classList.toggle('menu-open');
+    document.body.classList.toggle('lock');
   }
 }
